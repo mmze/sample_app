63a56c379347eb2fcdcae0f6e29d8f0d93158188
mmze <mmze@yandex.ru>
2018-12-18 10:53:40 +0300

    Heroku 3

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,10 +1,5 @@
-if Rails.env.production?
-  CarrierWave.configure do |config|
-    config.fog_credentials = {
-      :provider => 'AWS',
-      :aws_access_key_id => ENV['S3_ACCESS_KEY'],
-      :aws_secret_access_key => ENV['S3_SECRET_KEY']
-    }
-    config.fog_directory = ENV['S3_BUCKET']
+  if Rails.env.development? || Rails.env.test? || Rails.env.production?
+    CarrierWave.configure do |config|
+      config.storage = :file
+    end
   end
-end
